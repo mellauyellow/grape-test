46b3802e3ad77d380077a51cae781a7a5d1f7cc9
Melissa Lau <mellauyellow@gmail.com>
2017-03-31 14:53:21 -0700

    add PUT method to update cats

--- app/controllers/api/v1/cats.rb
+++ app/controllers/api/v1/cats.rb
@@ -18,6 +18,32 @@ module API
             Cat.find(params[:id])
           end
         end
+
+        desc "create a new cat"
+        params do
+          requires :name, type: String, desc: "cat name"
+          requires :age, type: Integer, desc: "cat age"
+        end
+        post do
+          Cat.create(
+            name: params[:name],
+            age: params[:age]
+          )
+        end
+
+        desc "update a cat"
+        params do
+          requires :id, type: Integer, desc: "cat id"
+          requires :name, type: String, desc: "cat name"
+          requires :age, type: Integer, desc: "cat age"
+        end
+        put do
+          current_cat = Cat.find(params[:id])
+          current_cat.update(
+            name: params[:name],
+            age: params[:age]
+          )
+        end
       end
     end
   end
